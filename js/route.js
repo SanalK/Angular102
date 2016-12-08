17b22a52e64cab7ed7c6e3a15c53f44526200727
SanalK <sanal_kishore@hotmail.com>
2016-12-08 11:10:04 -0500

    first commt

---
+++ js/route.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("london", {
+        templateUrl : "london.html"
+    })
+    .when("paris", {
+        templateUrl : "paris.html"
+    });
+});
+app.controller("londonCtrl", function ($scope) {
+    $scope.msg = "I love London";
+});
+app.controller("parisCtrl", function ($scope) {
+    $scope.msg = "I love Paris";
+});
